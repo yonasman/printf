80fe92347c29387827129678e86f9335096df7d1
Wasealex <wasealex27@gmail.com>
2023-10-13 16:06:37 +0300

    header file layout created named printf.h

---
+++ printf.h
@@ -0,0 +1,12 @@
+#ifndef PRINTF_H_
+#define PRINTF_H_
+/*header files*/
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/*function prototypes*/
+int _putchar(char c);
+
+#endif
