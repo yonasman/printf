ddc4eca008b64267bd160ac05eb786e69f335ee1
yonasman <yonasnegese250@gmail.com>
2023-10-13 07:15:34 -0700

    A test file

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - an Entry point
+ *@c: the char to be printed
+ *
+ *Return: Always 0(sucess)
+ */
+int main()
+{
+	char c = 't';
+
+	printf("%c\n", c);
+	return 0;
+}
