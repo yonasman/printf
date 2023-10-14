c13a8a8765c5ac3daa1d42ae8f6535f8d6d1419e
yonasman <yonasnegese250@gmail.com>
2023-10-14 05:47:26 -0700

    Our first printf project

--- test/test3.c
+++ test/test3.c
@@ -2,12 +2,12 @@
 int main()
 {
 	char w = 'w';
-	char *str = "hello";
+	char *str = "jeo";
 	_printf("hello\n");
 	_printf("hello %c\n", w);
 	_printf("hello %s\n", str);
 	_printf("hello %c %s\n", w, str);
-	_printf("hello %%");
+	_printf("hello %%\n");
 
 	return (0);
 }
