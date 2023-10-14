1a6f1d8b40482574a08721778e517ac854c2424d
Wasealex <wasealex27@gmail.com>
2023-10-14 11:19:32 +0300

    test folder created

---
+++ test/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/*header files*/
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/*function prototypes*/
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif/*MAIN_H*/
---
+++ test/test1.c
@@ -0,0 +1,10 @@
+#include "main.h"
+int main()
+{
+	char w = 'w';
+	int k = 7;
+	_printf("hello\n");
+	_printf("hello %c", w);
+	_printf("hello %d", d);
+	_printf("hello %c %d" w, d);
+}
