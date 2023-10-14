8bbbfa7a259742569a36298a6a1dd6f40e0f9ab4
yonasman <yonasnegese250@gmail.com>
2023-10-14 03:14:48 -0700

    printf first pro

--- test/test1.c
+++ test/test1.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
+int _printf(const char *format, ...);
 int main()
 {
 	char w = 'w';
-	int k = 7;
+	char *st ="HEY";
 	_printf("hello\n");
 	_printf("hello %c", w);
-	_printf("hello %d", k);
-	_printf("hello %c %d", w, k);
+	_printf("hello %s", st);
+	_printf("hello %c %s", w, st);
 	return 0;
 }
